c7b861081bcce91d8ff79b4676bdfdd28d8dbc60
tess <125449044+trisha-del@users.noreply.github.com>
2023-04-01 12:22:20 +0300

    Add files via upload

---
+++ interger.ino
@@ -0,0 +1,19 @@
+
+int in1 =34;
+int in2 =35;
+//motor 2 variables
+int enb=39;
+int enb =32;
+int in4 =33;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
